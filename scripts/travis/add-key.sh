909ce037fe8172abd27becf1b5e4a9e87162fb15
mordaroso <mordaroso@gmail.com>
2013-09-16 16:50:54 +0200

    remove cp command in add-key script

--- scripts/travis/add-key.sh
+++ scripts/travis/add-key.sh
@@ -3,4 +3,4 @@ security import ./scripts/travis/apple.cer -k ~/Library/Keychains/ios-build.keyc
 security import ./scripts/travis/dist.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
 security import ./scripts/travis/dist.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
-cp ./scripts/travis/profile/* ~/Library/MobileDevice/Provisioning\ Profiles/
+# cp ./scripts/travis/profile/* ~/Library/MobileDevice/Provisioning\ Profiles/
